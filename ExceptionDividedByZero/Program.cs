aa89dd18b499cb0cd63deffcd26f106b2aadc4ba
HP <HP@DESKTOP-N5QBMVO>
2021-09-04 15:28:19 +0530

    Add project files.

---
+++ ExceptionDividedByZero/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace ExceptionDividedByZero
+{
+    class Program
+    {
+        public void Divide()
+        {
+            int x, y, z = 0;
+            Console.WriteLine("Enter two numbers");
+
+
+            try
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                z = x / y;
+            }
+
+
+
+            catch (DivideByZeroException dze)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+
+            catch (FormatException fe)
+            {
+                Console.WriteLine("please check the values and try again");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(z);
+            }
+        }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Handling System Exception ");
+                DivideByZeroException obj = new DivideByZeroException();
+                obj.Divide();
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+
+
